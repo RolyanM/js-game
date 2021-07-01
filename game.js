ef5cdfa773a01eb41e849636c62b3a95e6d83831
Martin Naylor <martinnaylor88@gmail.com>
2021-07-01 09:52:09 +0100

    reset button added but not working

--- game.js
+++ game.js
@@ -14,38 +14,144 @@ let midThree = document.querySelector(".m3").value;
 //Bottom
 let btmOne = document.querySelector(".b1").value;
 let btmTwo = document.querySelector(".b2").value;
-let btmThree = document.querySelector(".b3").value;
+let btmThree = document.querySelector(".t3").value;
 
 
-document.addEventListener('DOMContentLoaded', () => {
 
-const squares = document.querySelectorAll('.container div')
-const playerDisplay = document.querySelector(".player")
-let currentPlayer = "";
-let playerOne = "Player One - Plant your flag!";
-let playerTwo = "Player Two - Plant your flag";
-
-
-squares.forEach(squares => {
-  squares.addEventListener("click", clickOutcome)
-})
-
-function clickOutcome(e) {
-  const squareArray = Array.from(squares)
-  const index = squareArray.indexOf(e.target)
-  console.log(index);
-  playerDisplay.innerHTML = currentPlayer
-
-
-  if(currnetPlayer === playerOne ) {
-    squares[index].classList.add(playerOne)
-    currentPlayer = playerTwo;
-    playerDisplay.innerHTML = currentPlayer
-  } else {
-    squares[index].classList.add(".playerTwo")
-    currentPlayer = playerOne;
-    playerDisplay.innerHTML = currentPlayer
-  }
+//function to place appropriate X or O in the correct box and then make it the other players go. 
+const playerTurn = () => {
+flag = 1;
+function topLeftPress() {
+    if (flag == 1) {
+        document.querySelector(".t1").innerHTML = "X";
+        document.querySelector(".t1").disabled = true;
+        flag = 0;
+    }
+    else {
+        document.querySelector(".t1").value = "0";
+        document.querySelector(".t1").disabled = true;
+        flag = 1;
+    }
+}
+  
+function topMiddlePress() {
+    if (flag == 1) {
+        document.querySelector(".t2").value = "X";
+        document.uerySelector(".t2").disabled = true;
+        flag = 0;
+    }
+    else {
+        document.querySelector(".t2").value = "0";
+        document.querySelector(".t2").disabled = true;
+        flag = 1;
+    }
+}
+  
+function topRightPress() {
+    if (flag == 1) {
+        document.querySelector(".t3").value = "X";
+        document.querySelector(".t3").disabled = true;
+        flag = 0;
+    }
+    else {
+        document.querySelector(".t3").value = "0";
+        document.querySelector(".t3").disabled = true;
+        flag = 1;
+    }
+}
+  
+function middleLeftPress() {
+    if (flag == 1) {
+        document.querySelector(".m1").value = "X";
+        document.querySelector(".m1").disabled = true;
+        flag = 0;
+    }
+    else {
+        document.querySelector(".m1").value = "0";
+        document.querySelector(".m1").disabled = true;
+        flag = 1;
+    }
+}
+  
+function middleMiddlePress() {
+    if (flag == 1) {
+        document.querySelector(".m2").value = "X";
+        document.querySelector(".m2").disabled = true;
+        flag = 0;
+    }
+    else {
+        document.querySelector(".m2").value = "0";
+        document.querySelector(".m2").disabled = true;
+        flag = 1;
+    }
+}
+  
+function middleRightPress() {
+    if (flag == 1) {
+        document.querySelector(".m3").value = "X";
+        document.querySelector(".m3").disabled = true;
+        flag = 0;
+    }
+    else {
+        document.querySelector(".m3").value = "0";
+        document.querySelector(".m3").disabled = true;
+        flag = 1;
+    }
+}
+  
+function bottomLeftPress() {
+    if (flag == 1) {
+        document.querySelector(".b1").value = "X";
+        document.querySelector(".b1").disabled = true;
+        flag = 0;
+    }
+    else {
+        document.querySelector(".b1").value = "0";
+        document.querySelector(".b1").disabled = true;
+        flag = 1;
+    }
+}
+  
+function bottomRightPress() {
+    if (flag == 1) {
+        document.querySelector(".b2").value = "X";
+        document.querySelector(".b2").disabled = true;
+        flag = 0;
+    }
+    else {
+        document.querySelector(".b2").value = "0";
+        document.querySelector(".b2").disabled = true;
+        flag = 1;
+    }
+}
+  
+function bottomLeftPress() {
+    if (flag == 1) {
+        document.querySelector(".t3").value = "X";
+        document.querySelector(".t3").disabled = true;
+        flag = 0;
+    }
+    else {
+        document.querySelector(".t3").value = "0";
+        document.querySelector(".t3").disabled = true;
+        flag = 1;
+    }
+}
 }
 
-})
+//reset button 
+
+// Function to reset game
+function resetButton() {
+  location.reload();
+  document.querySelector(".t1").value = '';
+  document.querySelector(".t2").value = '';
+  document.querySelector(".t3").value = '';
+  document.querySelector(".m1").value = '';
+  document.querySelector(".m2").value = '';
+  document.querySelector(".m3").value = '';
+  document.querySelector(".b1").value = '';
+  document.querySelector(".b2").value = '';
+  document.querySelector(".t3").value = '';
+
+}
